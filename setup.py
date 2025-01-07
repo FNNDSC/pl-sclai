48577e3bd73662e908e270aef03a6fcb59104159
rudolphpienaar <rudolph.pienaar@childrens.harvard.edu>
2025-01-07 17:23:58 +0000

    Apply bootstrap updates

--- setup.py
+++ setup.py
@@ -19,18 +19,18 @@ def get_version(rel_path: str) -> str:
 
 
 setup(
-    name='chris-plugin-template',
-    version=get_version('app.py'),
-    description='A ChRIS DS plugin template',
+    name='sclai',
+    version=get_version('sclai.py'),
+    description='A Simple Client for AI Interaction',
     author='FNNDSC',
-    author_email='dev@babyMRI.org',
-    url='https://github.com/FNNDSC/python-chrisapp-template',
-    py_modules=['app'],
+    author_email='rudolph.pienaar@childrens.harvard.edu',
+    url='https://github.com/FNNDSC/pl-sclai',
+    py_modules=['sclai'],
     install_requires=['chris_plugin'],
     license='MIT',
     entry_points={
         'console_scripts': [
-            'commandname = app:main'
+            'sclai = sclai:main'
         ]
     },
     classifiers=[
